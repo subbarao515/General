66cfe5397067c5328239af68f97a2716d0cacd9e
SubbaRao Anaparthi <SubbaRao_Anaparthi@ad.infosys.com>
2020-11-28 09:54:28 +0530

    NumbersPrintusingRecursion

---
+++ NumbersPrintRecursion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NumbersPrintRecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give a number");
+            int num=Convert.ToInt32(Console.ReadLine());
+            PrintNumbers(num);
+
+        }
+
+   public static void PrintNumbers(int number)
+    {
+           
+          if(number==0)
+          return;
+          else
+          {
+           PrintNumbers(number-1);
+           Console.WriteLine(number);
+          }
+
+    }
+    }
+
+
+
+   
+}
