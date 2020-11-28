9857b133c253f1899f3bc45fa8ae00b57ee92582
SubbaRao Anaparthi <SubbaRao_Anaparthi@ad.infosys.com>
2020-11-28 10:32:13 +0530

    Sumof digits

---
+++ sumofdigits/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace sumofdigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide your input number");
+            int num=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sum of the digits "+sumofdigits(num));
+        }
+
+        public static int  sumofdigits(int number)
+        {
+           if(number==0)
+           return 0;
+           else
+           {
+
+              return (number%10+sumofdigits(number/10)); 
+           }
+
+        }
+
+    }
+}
