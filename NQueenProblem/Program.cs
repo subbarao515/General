d657d21f87fa9e3184dc51dd43ed57047720db0b
SubbaRao Anaparthi <SubbaRao_Anaparthi@ad.infosys.com>
2020-12-01 22:32:17 +0530

    Decimal string print

--- NQueenProblem/Program.cs
+++ NQueenProblem/Program.cs
@@ -1,12 +1,91 @@
 ﻿using System;
-
+using System.Collections;
+using System.Collections.Generic;
 namespace NQueenProblem
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter ");
+            Console.WriteLine("Please enter Integer value for cheesboard ");
+              int boardSize=Convert.ToInt16(Console.ReadLine());
+            List<List<int>> completedBoard=Driver(boardSize);
+            string[][] outputString = new string[completedBoard.Count][];
+            int boardIndex = 0, rowIndex;
+            foreach (List<int> board in completedBoard)
+            {
+                rowIndex = 0;
+                outputString[boardIndex] = new string[boardSize];
+                foreach (int columnIndex in board)
+                {
+                    string strEachRow = string.Empty;
+                    for (int i=0;i<boardSize;i++)
+                    {
+                        if (i==columnIndex)
+                            strEachRow= strEachRow+"q";
+                        else
+                            strEachRow = strEachRow + "-";
+                    }
+                    outputString[boardIndex][rowIndex] = strEachRow;
+                    Console.WriteLine(strEachRow);
+                    rowIndex++;
+                }
+                Console.WriteLine("-----------------");
+                boardIndex++;
+            }
+
+            Console.Write("Completed ");
+            
+
         }
+
+        public static List<List<int>>  Driver(int boardSize)
+        {
+          List<List<int>> boards = new List<List<int>>();
+            List<int> board = new List<int>();
+            Helper(boardSize, boards, board,0);
+            return boards;
+
+        }
+
+    public static void Helper(int boardSize,List<List<int>> boards, List<int> board,int row) 
+    {
+ if (row == boardSize)
+            {
+                List<int> copyBoard = new List<int>(board);
+                boards.Add(copyBoard);
+                return;
+            }
+            else
+            {
+                for (int col = 0; col<boardSize; col++)
+                {
+                    if(IsSafeToPlaceQueenAt(board,row,col))
+                    {
+                        board.Add(col);
+                        Helper(boardSize, boards, board, row + 1);
+                        board.RemoveAt(board.Count - 1);
+                    }
+                }
+            }
+
+    }
+  public static bool IsSafeToPlaceQueenAt(List<int> board,int row,int column)
+        {
+            foreach(int QueenColumn in board)
+            {
+                if (QueenColumn == column)
+                    return false;
+            }
+            for(int QueenRow=0;QueenRow<board.Count;QueenRow++)
+            {
+                int XValue = Math.Abs(QueenRow - row);
+                int Yvalue = Math.Abs(Convert.ToInt32(board[QueenRow].ToString()) - column);
+                if (XValue == Yvalue)
+                    return false;
+            }
+            return true;
+        }
+
     }
 }
---
+++ PrintDecimalStrings/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace PrintDecimalStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter Decimal number");
+            int count=Convert.ToInt16(Console.ReadLine());
+            Driver(count);
+
+        }
+
+        static void Driver(int numDigit)
+        {
+           
+           Helper(numDigit,"");
+
+        }
+        static void Helper(int numDigit,string soFar)
+        {
+             if (numDigit == 0)
+            {
+                Console.WriteLine(soFar);
+            }
+            else
+            {
+              for(int choice=0;choice<=9;choice++)
+             {
+              Helper(numDigit-1,soFar+choice.ToString());
+             }
+                
+            }
+
+        }
+    }
+}
