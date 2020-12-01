b5c8b6328c521f7d370bf9a1a15e3bd8061a9bed
SubbaRao Anaparthi <SubbaRao_Anaparthi@ad.infosys.com>
2020-12-01 12:57:56 +0530

    Nqueens

---
+++ NQueenProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NQueenProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter ");
+        }
+    }
+}
